354c98c62190e875608898a6ef4c412182e14fcd
Kendall2106 <kenbrojim@gmail.com>
2023-06-09 03:06:02 -0600

    Modificar Home Listo

--- backend/AccesoADatos/Data/DataPagina.cs
+++ backend/AccesoADatos/Data/DataPagina.cs
@@ -7,5 +7,46 @@ namespace AccesoADatos.Data
 {
     public class DataPagina
     {
+        public async Task<List<Pagina>> mostrarPagina(string tipoPagina)
+        {
+            using (var _context = new DBContext())
+            {
+                var listaPagina = (from p in _context.pagina
+                                   join tp in _context.tipo_pagina on p.id_tipo_pagina equals tp.id_tipo_pagina
+                                   where tp.tipo == tipoPagina
+                                   select new Pagina
+                                   {
+
+                                       id_pagina = p.id_pagina,
+                                       id_tipo_pagina = p.id_tipo_pagina,
+                                       descripcion = p.descripcion,
+                                       imagen = p.imagen
+                                   });
+
+                return await listaPagina.ToListAsync();
+            }
+        }
+
+        public async Task<String> modificarPagina(Pagina pagina)
+        {
+
+            try
+            {
+                using (var _context = new DBContext())
+                {
+                    _context.Entry(pagina).State = EntityState.Modified;
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+
+                return "No se pueden guardar los cambios. " +
+                    "Vuelve a intentarlo y, si el problema persiste, " +
+                    "consulte con el administrador del sistema.";
+            }
+            return "Pagina Actualizada";
+
+        }
     }
 }
--- backend/Entidades/Entities/Pagina.cs
+++ backend/Entidades/Entities/Pagina.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,14 @@ namespace Entidades.Entities
 {
     public class Pagina
     {
-       public int? id { get; set; }
-       public int? idTipoPagina { get; set; }
-       public string? descripcion { get; set; }
-       public byte[]? imagen { get; set; }
+        [Key]
+        public int id_pagina { get; set; }
+
+        public int id_tipo_pagina { get; set; }
+
+        public string descripcion { get; set; }
+
+        public string? imagen { get; set; }
 
     }
 }
