1b83715ad9448abf0baf1d16b57e31b177097338
YarethLeal <yarethleal65@gmail.com>
2023-06-12 22:18:55 -0600

    Historia de Usuario 12

--- frontend/Admin-HotelCocoMilk/src/app/paginas/modificar-nosotros/modificar-nosotros.component.ts
+++ frontend/Admin-HotelCocoMilk/src/app/paginas/modificar-nosotros/modificar-nosotros.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Pagina } from 'src/app/core/modelos/pagina.model';
+import { PaginaService } from 'src/app/core/servicios/pagina.service';
 
 @Component({
   selector: 'app-modificar-nosotros',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./modificar-nosotros.component.css']
 })
 export class ModificarNosotrosComponent {
-
+  public dataAboutUs: Pagina;
+  //public galeria: Array<string>;
+  //public imagenPrincipal: string;
+  constructor(private paginaService: PaginaService) {
+    this.dataAboutUs = new Pagina(0);
+  }
+  ngOnInit(): void {
+    this.descripcionPagina("Sobre Nosotros");
+  }
+  descripcionPagina(buscar: string) {
+    this.paginaService.mostrarPagina({ tipoPagina: buscar }).subscribe((data: Pagina[]) => {
+      this.dataAboutUs = data[0];
+    });
+  }
+  modificarDescripcion(){
+    console.log(this.dataAboutUs);
+    this.paginaService.modificarPagina(this.dataAboutUs).subscribe((respuesta: string) => {
+      //this.respuesta = respuesta;
+      console.log(respuesta);
+    });
+  }
 }
