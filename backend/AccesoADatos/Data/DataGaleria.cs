95c7674e5cdd86d5c5321a9d5db5e4876bbcab4e
YarethLeal <yarethleal65@gmail.com>
2023-06-13 20:03:08 -0600

    Historia de Usuario 12 v2

---
+++ backend/AccesoADatos/Data/DataGaleria.cs
@@ -0,0 +1,59 @@
+﻿using AccesoADatos.Context;
+using Entidades.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccesoADatos.Data
+{
+    public class DataGaleria
+    {
+        public async Task<List<Galeria>> listarGaleria()
+        {
+            using (var _context = new DBContext())
+            {
+
+                return await _context.galeria.ToListAsync();
+            }
+        }
+        public async Task<String> registarImagenGaleria(Galeria galeria)
+        {
+            try
+            {
+                using (var _context = new DBContext())
+                {
+                    _context.galeria.Add(galeria);
+                    await _context.SaveChangesAsync();
+
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+
+                return "No se pueden guardar los cambios. " +
+                         "Vuelve a intentarlo y, si el problema persiste, " +
+                         "consulte con el administrador del sistema.";
+            }
+            return "Imagen Registrada";
+
+        }
+        public async Task<String> eliminarImagenGaleria(Galeria galeria)
+        {
+            using (var _context = new DBContext())
+            {
+
+                try
+                {
+                    _context.galeria.Remove(galeria);
+                    await _context.SaveChangesAsync();
+
+                }
+                catch (DbUpdateException /* ex */)
+                {
+                    return "No se puede eliminar. " +
+                         "Vuelve a intentarlo y, si el problema persiste, " +
+                         "consulte con el administrador del sistema.";
+                }
+                return "Imagen Eliminada";
+            }
+        }
+    }
+}
---
+++ backend/Admin-HotelCocoMilk/Controllers/GaleriaController.cs
@@ -0,0 +1,28 @@
+﻿using AccesoADatos.Data;
+using Entidades.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Admin_HotelCocoMilk.Controllers
+{
+    public class GaleriaController : Controller
+    {
+        [HttpGet]
+        [Route("listarGaleria")]
+        public async Task<List<Galeria>> listarGaleria()
+        {
+            return await (new DataGaleria().listarGaleria());
+        }
+        [HttpPost]
+        [Route("registrarImagenGaleria")]
+        public async Task<String> registarImagenGaleria(Galeria galeria)
+        {
+            return await (new DataGaleria().registarImagenGaleria(galeria));
+        }
+        [HttpPost]
+        [Route("eliminarImagenGaleria")]
+        public async Task<String> eliminarImagenGaleria(Galeria galeria)
+        {
+            return await (new DataGaleria().eliminarImagenGaleria(galeria));
+        }
+    }
+}
